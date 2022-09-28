a3c9132daf526e062729f9b987a6805247ad388b
yhuj79 <yu1085@naver.com>
2022-09-29 07:46:01 +0900

    fixed untracked files

--- .cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
-  "component---src-pages-about-tsx": () => import("./../../../src/pages/about.tsx" /* webpackChunkName: "component---src-pages-about-tsx" */),
-  "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */),
-  "component---src-templates-blog-post-tsx": () => import("./../../../src/templates/blogPost.tsx" /* webpackChunkName: "component---src-templates-blog-post-tsx" */)
-}
-
--- .cache/caches/gatsby-plugin-typography/typography.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("/Users/mac_yhj/IO/src/styles/typography")
